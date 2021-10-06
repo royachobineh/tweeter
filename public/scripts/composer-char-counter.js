3efb74a0cd3ab535b3afd6fdafa528da805e68d6
Roya Chobineh <roya.chbn@gmail.com>
2021-10-06 11:40:43 -0400

    tweet container updated

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,25 @@
-$(document).ready(function() {
-  let counter = $("output.counter");
-  $("#tweet-text").on('input', function() {
-    counter[0].value = 140 - $("#tweet-text").val().length;
-    if (counter[0].value < 0) {
-      counter.addClass("counterRed");
-    } else {
-      counter.removeClass("counterRed");
-    }
+$(function () {
+  $('textarea').on('input', function (event) {
+    $('.error').hide();
+
+    //Resize textarea on input
+    this.style.height = "";
+    this.style.height = this.scrollHeight + 'px';
+
+    //Decrements the counter on input
+    const counter = $(this).closest('form').find('.counter')[0];
+
+    counter.value = 140 - this.value.length;
+
+    //if counter exceeds character limit change the color to red
+    counter.value < 0 ? $(counter).css("color", "red") : $(counter).css("color", "#545149");
+  })
+
+  //resize textarea if window is resized
+  $(window).on('resize', function (event) {
+    const $textarea = $('textarea');
+
+    $textarea.height(0);
+    $textarea.height($textarea.prop('scrollHeight'));
   })
 });
